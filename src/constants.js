01a674821eb016dd98767f66d52163f468399fdc
vijaykanwal <vijaykanwal90@gmail.com>
2025-02-03 14:50:31 +0530

    fixed

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://13.203.160.31/api"
-export const BASE_URL = "http://localhost/api"
+export const BASE_URL = "http://13.203.160.31/api"
+// export const BASE_URL = "http://localhost/api"
