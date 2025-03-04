75230d8872c034f2e54868164df6eb48dd8c9261
vijaykanwal <vijaykanwal90@gmail.com>
2025-03-04 23:17:17 +0530

    added baseUrl

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "/api"
 // export const BASE_URL = "http://localhost:3000/api"
-export const BASE_URL = "https://dev-tinder-backend-mu.vercel.app/api"
+export const BASE_URL = "https://devtinderbackend-hbge.onrender.com/api"
 
---
+++ src/utils/utils.js
@@ -0,0 +1,2 @@
+//  const BASE_URL = 'https://devtinderbackend-hbge.onrender.com';
+// //  export { BASE_URL };
