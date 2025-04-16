7e15914dfbdb266690506afa91c04873194f3628
vijaykanwal <vijaykanwal90@gmail.com>
2025-04-16 21:34:30 +0530

    added base route

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,2 @@
- const BASE_URL = 'https://josh-backend.onrender.com/api'; // Replace with your actual base URL
+ const BASE_URL = 'https://devtinderbackend-6jr7.onrender.com/api'; // Replace with your actual base URL
 // //  export { BASE_URL };
