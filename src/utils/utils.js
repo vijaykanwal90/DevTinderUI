b892a1ccd432a82ca01fa56115ed9a75216f3fd7
vijaykanwal <vijaykanwal90@gmail.com>
2025-04-16 22:11:11 +0530

    added port

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,2 @@
- const BASE_URL = 'https://devtinderbackend-6jr7.onrender.com:3000/api'; // Replace with your actual base URL
+ const BASE_URL = 'https://devtinderbackend-6jr7.onrender.com/api'; // Replace with your actual base URL
 // //  export { BASE_URL };
