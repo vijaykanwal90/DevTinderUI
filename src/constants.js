821152a12c38f926f886f028d4ca8e1ecd18bb97
vijaykanwal <vijaykanwal90@gmail.com>
2025-10-02 19:50:11 +0530

    changed to https

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 const env = 'development'; // Corrected spelling
-export const BASE_URL = env === 'development' ? "http://194.164.150.12:5000/api" : "https://devtinderbackend-6jr7.onrender.com/api";
+export const BASE_URL = env === 'development' ? "https://194.164.150.12:5000/api" : "https://devtinderbackend-6jr7.onrender.com/api";
